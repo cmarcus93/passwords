2fbaa3e7ff2fa978fce53bd55c0a4e556aa02efa
cmarcus93 <98795812+cmarcus93@users.noreply.github.com>
2023-01-27 17:56:55 +1030

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-$settings['ldap_host']        = 'ldaps://server:636';
-$settings['ldap_username']    = 'user';
-$settings['ldap_password']    = 'password';
-$settings['ldap_domain']      = 'domain'; //netbios domain
-$settings['ldap_studentou']   = 'OU=Students,DC=domain,DC=sa,DC=edu,DC=au';
-$settings['ldap_staffou']     = 'OU=Staff,DC=domain,DC=sa,DC=edu,DC=au';
+$settings['ldap_host']        = 'ldaps://0780SVRDC1:636';
+$settings['ldap_username']    = 'password';
+$settings['ldap_password']    = 'U7Epdy57QCF$';
+$settings['ldap_domain']      = 'millicenths.local'; //netbios domain
+$settings['ldap_studentou']   = 'OU=Students,OU=Curriculum,DC=millicenths,DC=local';
+$settings['ldap_staffou']     = 'OU=Staff,OU=Curriculum,DC=millicenths,DC=local';
 //not used plans though
-$settings['ldap_studentsg']   = 'CN=Students,OU=Students,DC=domain,DC=sa,DC=edu,DC=au';
+$settings['ldap_studentsg']   = 'CN=SG-Students,OU=Groups,OU=Curriculum,DC=millicenths,DC=local';
 //security group staff with reset permissions are in
-$settings['ldap_resetsg']     = 'CN=Classroom Teachers,OU=Students,DC=domain,DC=sa,DC=edu,DC=au';
+$settings['ldap_resetsg']     = 'CN=Password Reset,OU=Groups,OU=Curriculum,DC=millicenths,DC=local';
